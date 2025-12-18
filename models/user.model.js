1111340be224230c174e12e9a2efb14fcfcd6e1f
saeeda-09 <saeedamuhtashima@gmail.com>
2025-12-18 17:19:45 +0600

    Add files via upload

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema(
     {
@@ -35,6 +36,18 @@ const UserSchema = mongoose.Schema(
         timestamps: true // Adds createdAt and updatedAt fields
     }
 );
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    });
+
+    UserSchema.methods.comparePassword = async function (candidatePassword) {
+        return await bcrypt.compare(candidatePassword, this.password);
+    };
+
 
 const User = mongoose.model("User", UserSchema);
 
