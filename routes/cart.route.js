c19e0a17ab87a27f3a7c201632dd05f460a46b80
Moon <amina03noor@gmail.com>
2025-12-19 21:07:13 +0600

    Update cart.route.js
    added authentication

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,7 +1,9 @@
-import express from "express"
-import {getCarts, getCart, createCart, updateCart, deleteCart} from "../controllers/cart.controller.js"
+import express from "express";
+import {getCarts, getCart, createCart, updateCart, deleteCart} from "../controllers/cart.controller.js";
+import {authenticate} from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+router.use(authenticate);
 
 router.get("/", getCarts);
 router.get("/:id", getCart);
@@ -9,4 +11,4 @@ router.post("/", createCart);
 router.put("/:id", updateCart);
 router.delete("/:id", deleteCart);
 
-export default router
+export default router;
