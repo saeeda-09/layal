87e74a36684c212dbd456cd5e44eb860392a60e7
saeeda-09 <saeedamuhtashima@gmail.com>
2026-01-16 10:11:37 +0600

    Add files via upload

--- api/models/product.model.js
+++ api/models/product.model.js
@@ -36,31 +36,3 @@ const ProductSchema = mongoose.Schema(
 const Product = mongoose.model("Product", ProductSchema);
 
 module.exports = Product;
-/*import mongoose from 'mongoose';
-
-const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true 
-    },
-    price: {
-        type: Number,  
-        required: true
-    },
-    description: { 
-        type: String
-    },
-    category: {
-        type: String
-    },
-    quantity: {
-        type: Number,
-        required: true
-    },
-    image: {
-        type: String,
-        default: ""
-    }
-}, { timestamps: true });
-export const Product = mongoose.model('Product', productSchema);
-//module.exports = Product;*/
